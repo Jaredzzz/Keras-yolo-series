db303249b77227f261ae2ad9defbb6ec9b1cf475
Jaredzzz <48994797+Jaredzzz@users.noreply.github.com>
2021-07-19 10:58:04 +0800

    Update and rename core/activation.py to 核/activation.py

--- 核/activation.py
+++ 核/activation.py
@@ -34,3 +34,26 @@ class Mish(Layer):
         
         return input_shape
 
+class Mish6(Layer):
+    '''
+    Mish6 Activation Function.
+    '''
+
+    def __init__(self, **kwargs):
+        super(Mish6, self).__init__(**kwargs)
+        self.supports_masking = True
+
+    def call(self, inputs):
+        x1 = tf.minimum(tf.maximum(0.0, inputs * K.tanh(K.softplus(inputs))), 6.0)
+        x2 = inputs * K.tanh(K.softplus(inputs))
+        x = tf.where(inputs > 0.0, x1, x2)
+        return x
+
+    def get_config(self):
+        config = super(Mish6, self).get_config()
+        return config
+
+    def compute_output_shape(self, input_shape):
+
+        return input_shape
+
